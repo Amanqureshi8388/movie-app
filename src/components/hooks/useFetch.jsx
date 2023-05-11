23618065a7d5d00350f3c6d003187f3a69d43857
Aman qureshi <Amanqureshi8388@gmail.com>
2023-05-11 23:37:08 +0530

    update

--- src/components/hooks/useFetch.jsx
+++ src/components/hooks/useFetch.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "../utils/api";
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
+    const [data, setData] = useState([]);
     const [loading, setLoading] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
         setLoading("loading...");
-        setData(null);
+        setData([]);
         setError(null);
 
         fetchDataFromApi(url)
